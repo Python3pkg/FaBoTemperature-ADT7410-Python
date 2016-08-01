db463d473e61051816dc810e1349015d19ec58c9
Shimakage <shimakage@gclue.jp>
2016-08-01 15:01:36 +0900

    change for doxygen

--- FaBoTemperature_ADT7410/ADT7410.py
+++ FaBoTemperature_ADT7410/ADT7410.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-## @package FaBoTemperature_ADT7410
-#  This is a library for the FaBo Temperature I2C Brick.
+## @package ADT7410
+#  This is a FaBoTemperature_ADT7410 library for the FaBo Temperature I2C Brick.
 #
 #  http://fabo.io/207.html
 #
@@ -16,35 +16,56 @@ import time
 # ADT7410 Default I2C Slave Address
 SLAVE_ADDRESS     = 0x48
 
-## Register Addresses
+# Register Addresses
+## Temp MSB Register
 TEMP_MSB_REG      = 0x00
+## Temp LSB Register
 TEMP_LSB_REG      = 0x01
+## Status Register
 STATUS_REG        = 0x02
+## Config Register
 CONFIGURATION_REG = 0x03
+## WHO_AM_I Register
 WHO_AM_I_REG      = 0x0B
 
-## Config Parameter
-BIT13_RESOLUTION      = 0b00000000
-BIT16_RESOLUTION      = 0b10000000
+# Config Parameter
+## BIT13_RESOLUTION   : 0b00000000
+BIT13_RESOLUTION      = 0x00
+## BIT16_RESOLUTION : 0b10000000
+BIT16_RESOLUTION      = 0x80
 
-OP_MODE_CONTINUOUS    = 0b00000000
-OP_MODE_ONESHOT       = 0b00100000
-OP_MODE_SPS           = 0b01000000
-OP_MODE_SHUTDOWN      = 0b01100000
+## OP_MODE_CONTINUOUS : 0b00000000
+OP_MODE_CONTINUOUS    = 0x00
+## OP_MODE_ONESHOT    : 0b00100000
+OP_MODE_ONESHOT       = 0x20
+## OP_MODE_SPS        : 0b01000000
+OP_MODE_SPS           = 0x40
+## OP_MODE_SHUTDOWN   : 0b01100000
+OP_MODE_SHUTDOWN      = 0x60
 
-INTERRUPT_MODE        = 0b00000000
-COMPARATOR_MODE       = 0b00010000
+## INTERRUPT_MODE     : 0b00000000
+INTERRUPT_MODE        = 0x00
+## COMPARATOR_MODE    : 0b00010000
+COMPARATOR_MODE       = 0x10
 
-INT_LOW               = 0b00000000
-INT_HIGH              = 0b00001000
+## INT_LOW            : 0b00000000
+INT_LOW               = 0x00
+## INT_HIGH           : 0b00001000
+INT_HIGH              = 0x08
 
-CT_LOW                = 0b00000000
-CT_HIGH               = 0b00000100
+## CT_LOW             : 0b00000000
+CT_LOW                = 0x00
+## CT_HIGH            : 0b00000100
+CT_HIGH               = 0x04
 
-BIT16_OP_MODE_1FAULT  = 0b00000000
-BIT16_OP_MODE_2FAULT  = 0b00000001
-BIT16_OP_MODE_3FAULT  = 0b00000010
-BIT16_OP_MODE_4FAULT  = 0b00000011
+## BIT16_OP_MODE_1FAULT : 0b00000000
+BIT16_OP_MODE_1FAULT  = 0x00
+## BIT16_OP_MODE_2FAULT : 0b00000001
+BIT16_OP_MODE_2FAULT  = 0x01
+## BIT16_OP_MODE_3FAULT : 0b00000010
+BIT16_OP_MODE_3FAULT  = 0x02
+## BIT16_OP_MODE_4FAULT : 0b00000011
+BIT16_OP_MODE_4FAULT  = 0x03
 
 ## smbus
 bus = smbus.SMBus(1)
@@ -103,12 +124,3 @@ class ADT7410:
 
         else:
             return 0.0
-
-if __name__ == "__main__":
-    adt7410 = ADT7410()
-
-    while True:
-        temp = adt7410.read()
-        print "Temperature = ", temp
-        print
-        time.sleep(1)
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # FaBoTemperature-ADT7410-Python
+
+## How to install.
+
+```
+pip install FaBoTemperature_ADT7410
+```
+
+## FaBo Temperature I2C Brick
+
+　[#207 Temperature I2C Brick](http://fabo.io/207.html)
+
+## ADT7410
+
+　ADT7410 is 16-Bit Digital I2C Temperature Sensor.
+
+### ADT7410 Datasheet
+
+　[ADT7410 Datasheet](http://www.analog.com/media/en/technical-documentation/data-sheets/ADT7410.pdf)
+
+## Releases
+
+- 1.0.0 Initial release.
